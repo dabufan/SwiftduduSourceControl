b20fc7f3c7c44eb7bed81fb41f33b00e6baf13d7
dudu <dabufan@gmail.com>
2025-05-26 16:40:48 +0800

    Initial Commit

---
+++ SwiftduduSourceControl/SwiftduduSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftduduSourceControlApp.swift
+//  SwiftduduSourceControl
+//
+//  Created by Q Lee on 2025/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftduduSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
