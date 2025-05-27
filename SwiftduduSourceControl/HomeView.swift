83b6eee2865dc897b30d2d4fa0a07a01f3664b7f
dudu <dabufan@gmail.com>
2025-05-27 08:59:32 +0800

    Add Homeview

---
+++ SwiftduduSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftduduSourceControl
+//
+//  Created by Q Lee on 2025/5/27.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
