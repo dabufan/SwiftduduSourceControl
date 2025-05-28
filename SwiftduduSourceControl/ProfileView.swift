2babf66c1ed1dcf84be6a4fbd0729e9bb9728e3b
dabufan <152623053+dabufan@users.noreply.github.com>
2025-05-28 11:38:01 +0800

    Release/release 1.2.0 (#7)
    * [Feature] Add ProfileView
    
    * [Feature] Add ispremium bool
    
    * [Release]1.2.0(4)
    
    * [Bug] fix a bug

---
+++ SwiftduduSourceControl/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  SwiftduduSourceControl
+//
+//  Created by Q Lee on 2025/5/28.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @State var isPremiuml: Bool = false
+    var body: some View {
+        Text("Nick")
+            .onAppear {
+                
+            }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
