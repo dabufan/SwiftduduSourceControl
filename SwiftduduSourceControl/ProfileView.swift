e3303841e47b3483dc538b7ccdfaeacbad6b8d4f
dudu <dabufan@gmail.com>
2025-05-28 11:15:54 +0800

    [Feature] Add ProfileView

---
+++ SwiftduduSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftduduSourceControl
+//
+//  Created by Q Lee on 2025/5/28.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Nick")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
